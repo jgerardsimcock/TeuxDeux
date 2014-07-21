25d2e56c0a2ad054fed75b941ecbcc81001ceda4
jgerardsimcock <justin_simcock@yahoo.com>
2014-07-20 17:15:24 -1000

    Server running, first post up

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var jade = require('jade');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+var app = express();
+
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json())
+app.use(methodOverride());
+
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
