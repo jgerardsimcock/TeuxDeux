83c2958651d9252493be3b4b4ddc987e958e4696
jgerardsimcock <justin_simcock@yahoo.com>
2014-07-23 18:13:35 -1000

    tasks started

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+
+//THESE VARIABLES LINK TO DEPENDENCIES IN THE PACKAGE JSON.
 var express = require('express');
 var jade = require('jade');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+//THIS VARIABLE IS SO WE CAN CALL METHODS IN EXPRESS ON THE OUR APPLICATION
 var app = express();
 
 
-// app.get('/', function(req, res){
-//   res.send('Hello World');
-// });
+
+//THIS CREATES A NEW SCHEMA IN MONGOOSE
+var Schema = mongoose.Schema
+
+var teuxdeuxSchema = new Schema({
+  task_input : String,
+  created_at: Date
+
+});
+
+var teuxDeux = mongoose.model('tasks', teuxdeuxSchema);
 
 //Get
 
@@ -16,8 +27,28 @@ app.get('/', function(req, res){
   res.render('tasks/list.jade');
 });
 
+app.get('/', function(req, res){
+  res.render('tasks/list.jade');
+});
+
+app.get('/', function(req, res){
+  res.render('tasks/list.jade');
+});
+
 //POST  
 
+app.post('/tasks'. function(req,res){
+  // var task = new TaskItem(){
+  //   // title = req.param('title'),
+  //   // notes: req.param('notes')
+
+  //  // look at docs for params in express
+
+  //   // task.save(function(wert, task){
+  //   //   if(wert){res.send(500, wert);}
+  //   //   res.redirect{'/'};
+  //   })
+})
 //UPDATE
 
 //DELETE
@@ -28,8 +59,12 @@ app.set('views', __dirname + '/templates');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.del('/', function(req,res))
 app.use(bodyParser.json())
 app.use(methodOverride());
+
+
 
 
 var server = app.listen(3000, function() {
