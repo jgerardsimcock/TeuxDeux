77166a1009fbb917770bcb9643b16b5ea96515dc
jgerardsimcock <justin_simcock@yahoo.com>
2014-07-26 19:20:59 -1000

    Task Count Works

--- public/js/jqueryStuff.js
+++ public/js/jqueryStuff.js
@@ -1,7 +1,8 @@
 $(function() {
-  
   var checkbox = $(".checkbox");
   checkbox.click(function(){
       $(this).parent().submit();
   });
+   var n = $(".incomplete").length;
+   $('#taskCount').html("You have " + n + " tasks");
 }); 
